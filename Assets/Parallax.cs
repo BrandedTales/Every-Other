df96b23b3b274af746be9b8431b94d7680d7434e
BrandedTales <lordbrand@gmail.com>
2019-08-14 00:30:18 -0500

    Updated Tilesets and added parallax mist.

---
+++ Assets/Parallax.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class Parallax : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            lastpos = camera.position;
+        }
+
+        [SerializeField] Transform camera;
+        [SerializeField] float speedCoefficient;
+        Vector3 lastpos;
+
+        void Update()
+        {
+            transform.position -= ((lastpos - camera.position) * speedCoefficient);
+            lastpos = camera.position;
+        }
+    }
+}
